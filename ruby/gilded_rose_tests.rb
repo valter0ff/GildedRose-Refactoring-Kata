ad1f843ab10b93aa8815b123ed137bf809643e03
Valerii Pohrebniak <km270985pvg@gmail.com>
2025-03-18 02:16:58 +0200

    move test to spec folder, update test

--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative 'gilded_rose'
 
 class TestGildedRose < Minitest::Test
   def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal "fixme", items[0].name
+    items = [Item.new('foo', 0, 0)]
+    GildedRose.new(items).update_quality
+    assert_equal 'fixme', items[0].name
   end
 end
